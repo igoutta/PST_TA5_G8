c8c95961b057acefff390d117623a05eecb3a6ae
pvibarra <pvibarra@espol.edu.ec>
2020-08-12 15:59:38 -0500

    Prueba 5

--- app/src/main/java/com/example/amst8/MainActivity.java
+++ app/src/main/java/com/example/amst8/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //comentario Carlos//
+        //comentario
         finish();
 
     }

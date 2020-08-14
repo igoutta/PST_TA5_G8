33a774edea0951fd5492bd915f0e3be824eda8c4
pvibarra <pvibarra@espol.edu.ec>
2020-08-14 00:03:28 -0500

    Agg de libros categoría Ficción a db

--- app/src/main/java/com/example/amst8/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/example/amst8/AdminSQLiteOpenHelper.java
@@ -29,6 +29,15 @@ public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
         db.execSQL("insert into libro values ('0001', 'Los Juegos de Ender', 'Orson Scott Card','1985'," +
                 "'Aqui va la descripcion'," +
                 "'juego_de_ender.jpg', 1)");
+        db.execSQL("insert into libro values ('0002', 'Harry Potter y la Orden del Fénix', 'J.K. Rowling','2003'," +
+                "'En su quinto año en Hogwarts, Harry descubre que muchos integrantes de la comunidad de magos no conocen la verdad acerca de su encuentro con Lord Voldemort. Cornelius Fudge, ministro de magia, designa a Dolores Umbridge como maestra de defensa contra de las artes oscuras porque cree que el profesor Dumbledore planea apoderarse de su trabajo. Pero sus enseñanzas son inadecuadas, por lo que Harry prepara a los estudiantes para defender la escuela en contra del mal.'," +
+                "'harry_potter.jpg', 1)");
+        db.execSQL("insert into libro values ('0003', 'Percy Jackson y El Ladrón del Rayo', 'Rick Riordan','2005'," +
+                "'El ladrón del rayo es un libro escrito por Rick Riordan. Es el primer libro de la serie Percy Jackson y los dioses del Olimpo, que cuenta las aventuras de un chico actual de doce años, Percy Jackson, cuando descubre que es un semidiós; el hijo de una mortal, Sally Jackson, y del dios griego, Poseidón.'," +
+                "'percy_jackson.jpg', 1)");
+        db.execSQL("insert into libro values ('0004', 'Los Juegos del Hambre: Sinsajo', 'Suzanne Collins','2010'," +
+                "'Sinsajo es una novela de ciencia ficción distópica para jóvenes adultos de 2010 y el tercer libro de la trilogía de Los juegos del hambre, de la autora Suzanne Collins. Después de destruir los juegos para siempre, Katniss llega al Distrito 13 para salvar a Peeta y una nación conmovida por su coraje.'," +
+                "'juegos_del_hambre.jpg', 1)");
     }
 
     @Override
